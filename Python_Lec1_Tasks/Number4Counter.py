eb087ad7b375024507061dcf5708d96caa13dce1
mahmoud moheb <42515396+mahmoud-moheb@users.noreply.github.com>
2023-11-12 15:52:38 +0200

    Add files via upload

---
+++ Python_Lec1_Tasks/Number4Counter.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+#A program that count how many times that number 4 repeated in a given list
+lst=list(input("enter your list: "))
+print("the number FOUR repeated: "+str(lst.count('4'))+" times in the list")
---
+++ Python_Lec1_Tasks/figcharacters.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import pyfiglet
+text=input("Enter your text: ")
+print(pyfiglet.figlet_format(text))
