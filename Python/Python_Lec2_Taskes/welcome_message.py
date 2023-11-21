33fc873c5c78e03653f0b8e05d9b295da65ccf39
mahmoud moheb <42515396+mahmoud-moheb@users.noreply.github.com>
2023-11-21 15:39:33 +0200

    Delete Python/Python_Lec2_Taskes directory

--- Python/Python_Lec2_Taskes/welcome_message.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from gtts import gTTS
-import vlc
-myobj = gTTS(text = "مرحبا بك يا معلم", lang = "ar", slow = False )
-myobj.save("welcome.mp4")
-p = vlc.MediaPlayer("./welcome.mp4")
-p.play()
-while True:
-    pass
